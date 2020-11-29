041b32dfbabc541b4625365722921796bfdc6c47
peterx <xu.kang@husky.neu.edu>
2020-11-29 18:03:58 -0500

    add session timeout

--- server.js
+++ server.js
@@ -19,7 +19,8 @@ app.use(cors({
 app.use(session({
     resave: false, // don't save session if unmodified
     saveUninitialized: false, // don't create session until something stored
-    secret: 'EML8MnuXDvts02hPFNvuBijBDBCxmbu2ld'
+    secret: 'EML8MnuXDvts02hPFNvuBijBDBCxmbu2ld',
+    cookie: {maxAge: 1000 * 3600}
 }));
 
 const uri = process.env.ATLAS_URI;
