d53aa56ceb3855e4d54debad9fbf0155f329b59d
peterx <xu.kang@husky.neu.edu>
2020-12-04 12:53:47 -0500

    remove some useless comments

--- models/music.model.js
+++ models/music.model.js
@@ -4,7 +4,7 @@ const musicSchema = mongoose.Schema(
         title: String,
         musicId: String,
     },
-    {collection: 'music'});
+    {collection: 'musics'});
 
 const Music = mongoose.model('Music', musicSchema)
 module.exports = Music;
