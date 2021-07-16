6e9fffa70f929fc5f32fa095ea123b79697c9060
peter <xukangming111@gmail.com>
2021-07-15 20:00:38 -0400

    back

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use(session({
     // cookie: {}
 
 }));
-app.set('trust proxy', 1);
+// app.set('trust proxy', 1);
 const uri = process.env.ATLAS_URI;
 console.log(uri);
 mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true,  useUnifiedTopology:
